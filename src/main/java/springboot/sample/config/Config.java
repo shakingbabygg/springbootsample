64246afef0e91c8e70d494ba2f269a79a5588ef8
HowardChen <howard.chen@e-utc.com.tw>
2022-10-15 13:29:30 +0800

    load application prop

--- src/main/java/springboot/sample/config/Config.java
+++ src/main/java/springboot/sample/config/Config.java
@@ -1,6 +1,7 @@
 package springboot.sample.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -14,6 +15,21 @@ public class Config {
 	@Autowired
 	private Environment env;
 
+	@Value("db.jdbc")
+	private String jdbc;
+
+	@Value("db.driverClass")
+	private String driverClass;
+
+	@Value("db.dialect")
+	private String dialect;
+
+	@Value("db.user")
+	private String user;
+
+	@Value("db.pswd")
+	private String pswd;
+
 	public String get(String key) {
 		try {
 			return env.getProperty(key);
@@ -22,4 +38,5 @@ public class Config {
 			return "";
 		}
 	}
+
 }
